dc58fb185d08cc45db24f154f9062faf8a62fc60
28LucasJustino <lucasjust28@gmail.com>
2024-02-27 17:38:42 -0300

    terminar de fazer o resevar e cadastrar

---
+++ js/reservar.js
@@ -0,0 +1,49 @@
+/*
+const biblioteca = [
+    {
+        tipo: "Carro",
+        marca: "Volkswagen",
+        nome: "Golf",
+        ano: "2005",
+        cor: "Prata",
+        placa: "ABC-1222",
+        diaria: "R$170,00"
+    },
+    {
+        tipo: "Carro",
+        marca: "Chevrolet",
+        nome: "Ômega",
+        ano: "1998",
+        cor: "Preto",
+        placa: "DBC-1422",
+        diaria: "R$120,00"
+    },
+    {
+        tipo: "Carro",
+        marca: "Volkswagen",
+        nome: "Gol Bolinha  ",
+        ano: "2000",
+        cor: "Prata",
+        placa: "ABC-1222",
+        diaria: "R$140,00"
+    },
+    {
+        tipo: "Carro",
+        marca: "Volkswagen",
+        nome: "Logus",
+        ano: "1999",
+        cor: "Branco",
+        placa: "DBC-1422",
+        diaria: "R$110,00"
+    },
+];
+
+document.addEventListener("DOMContentLoaded", function () {
+    const tbody = document.getElementById("corpo-tabela");
+    for (i=0; i < listaDeCarros.length; i++) {
+        const linhaAtual = document.createElement("tr");
+        linhaAtual.innerHTML = `<td>${listaDeCarros[i].tipo}</td><td>${listaDeCarros[i].marca}</td><td>${listaDeCarros[i].nome}</td><td>${listaDeCarros[i].ano}</td><td>${listaDeCarros[i].cor}</td><td>${listaDeCarros[i].placa}</td><td>${listaDeCarros[i].diaria}</td>`;
+        tbody.appendChild(linhaAtual);
+    }
+});
+*/
